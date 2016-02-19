46ce3431269b53851a4eefee24046abd60e04790
Jérémie Litzler <webdev.jl@gmail.com>
2016-02-19 06:49:00 +0100

    #1

---
+++ src/Codes/ControllerErrors.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * ControllerErrors
+ * 
+ * @author Jeremie Litzler
+ * @copyright Copyright (c) 2015
+ * @licence http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link https://github.com/puzzlout/Exceptions
+ * @since Version 1.0.0
+ * @package ControllerErrors
+ */
+
+namespace Puzzlout\Exceptions\Codes;
+
+class ControllerErrors {
+    const ActionNotExistForController = 0;
+}
